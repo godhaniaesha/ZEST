1002e3fd745f01c32f995aeeaa572c0f0029bdbc
Krupali2626 <krupalikumbhani26@gmail.com>
2026-05-23 09:24:45 +0530

    Add About Us page and update routing in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import '../src/styles/d_style.css';
@@ -7,12 +6,15 @@ import '../src/styles/x_style.css';
 import '../src/styles/z_style.css';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import AboutUs from './pages/AboutUs';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navbar />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+
       </Routes>
     </BrowserRouter>
   );
