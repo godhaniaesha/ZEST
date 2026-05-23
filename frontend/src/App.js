f98f46c66064a8f8944792be69919ebed31d9bdc
godhaniaesha <aesha.kalathiyainfotech@gmail.com>
2026-05-23 09:47:11 +0530

    app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,44 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import '../src/styles/d_style.css';
 import '../src/styles/h_style.css';
 import '../src/styles/x_style.css';
 import '../src/styles/z_style.css';
+
 import Home from './pages/Home';
 import ContactUs from './pages/ContactUs';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Auth from './components/Auth';
 import AboutUs from './pages/AboutUs';
-import AppRoutes from '../src/admin/Approutes';
+
+import AppRoutes from './admin/Approutes';
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+
+      {/* <Navbar /> */}
+
       <div style={{ marginTop: '70px' }}>
+
+        {/* User Routes */}
         <Routes>
+          <Route path="/navbar" element={<Navbar />} />
           <Route path="/auth" element={<Auth />} />
-          <Route path="/" element={<Navbar />} />
           <Route path="/home" element={<Home />} />
           <Route path="/aboutus" element={<AboutUs />} />
-          <AppRoutes />
+          <Route path="/contactus" element={<ContactUs />} />
         </Routes>
+
+        {/* Admin Routes */}
+        <AppRoutes />
+
       </div>
+
       {/* <Footer /> */}
+
     </BrowserRouter>
   );
 }
