6fa9a7e14693477172826499100d1e8ba6216b54
denishababariya <denishababariya899@gmail.com>
2026-06-06 09:59:27 +0530

    feat: define STAFF_ROLES array for role-based access control

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,14 @@ const crypto = require('crypto');
 
 const dns = require ('node:dns');
 dns.setServers(['8.8.8.8', '8.8.4.4']);
-
+const STAFF_ROLES = [
+  'superadmin',
+  'manager',
+  'chef',
+  'waiter',
+  'cashier',
+  'bartender'
+];
 if (!global.crypto) {
   global.crypto = crypto;
 }
